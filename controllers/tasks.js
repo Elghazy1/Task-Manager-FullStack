41f08643d85fd682dcbdc942db434b39c9bb905d
Elghazy1 <yazidelghazy@gmail.com>
2021-10-19 22:40:34 +0200

    delete task

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -21,7 +21,6 @@ const getTask = async (req, res) => {
   try {
     const { id: taskID } = req.params;
     const task = await Task.findOne({ _id: taskID });
-
     if (!task) {
       return res.status(404).json({ msg: `No task with id : ${taskID}` });
     }
@@ -36,8 +35,18 @@ const updateTask = (req, res) => {
   res.send("Update task");
 };
 
-const deleteTask = (req, res) => {
-  res.send("Delete task");
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskID });
+
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id : ${taskID}` });
+    }
+    res.status(200).json({ task });
+  } catch {
+    res.status(500).json({ msg: error });
+  }
 };
 
 module.exports = {
